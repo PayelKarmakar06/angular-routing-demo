1a7e230ea9ffd620ce24776615824188c1884053
PayelKPwC <payel.karmakar@pwc.com>
2020-04-21 10:00:36 +0530

    basic lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "about",
+    loadChildren: "./about/about.module#AboutModule"
+  },
+  {
+    path: "viewdetails",
+    loadChildren: "./viewdetails/viewdetails.module#ViewdetailsModule"
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
